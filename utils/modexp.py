e3bd304c517c5157b50c0555180b0bbdfb2925c5
bnmfw <bernardoborgessandoval@gmail.com>
2024-09-26 15:47:15 -0300

    fast exponentiation
    Signed-off-by: bnmfw <bernardoborgessandoval@gmail.com>

--- utils/modexp.py
+++ utils/modexp.py
@@ -1,16 +1,16 @@
 """
-Implementa exponenciacao modular (provavelmente) segura
+Implementa exponenciacao modular rapida
 """
-
 
 def modexp(base, exp, mod):
     result = 1
-    base = base % mod  # Take mod of base to simplify the computation
+    base = base % mod 
     while exp > 0:
-        # If exp is odd, multiply base with the result
-        if exp % 2 == 1:
+        
+        if exp % 2:
             result = (result * base) % mod
-        # Square the base and halve the exponent
+        
         base = (base * base) % mod
         exp //= 2
+    
     return result
