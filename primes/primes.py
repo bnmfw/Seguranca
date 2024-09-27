b75cc1b800c3147be95b7241832d814412cd697b
bnmfw <bernardoborgessandoval@gmail.com>
2024-09-27 18:29:58 -0300

    primes_ultil works correctly now
    Signed-off-by: bnmfw <bernardoborgessandoval@gmail.com>

--- primes/primes.py
+++ primes/primes.py
@@ -2,6 +2,8 @@
 Implementa um algoritmo que calcula todos os primos ate n
 """
 
+from bisect import bisect_left
+
 
 class Primes:
     def __init__(self) -> None:
@@ -9,7 +11,7 @@ class Primes:
 
     def __call__(self, value) -> list:
         if value < self.primes[-1]:
-            return self.primes
+            return self.primes[: bisect_left(self.primes, value)]
 
         for i in range(self.primes[-1] + 2, value + 1):
             is_prime = True
@@ -29,3 +31,4 @@ primes_until = Primes()
 
 if __name__ == "__main__":
     print(primes_until(1000))
+    print(primes_until(6))
